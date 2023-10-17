9c5aa1fde0d490a8d7310fe2262d41959e9d3949
raimundo <raimundo.n.neto10@gmail.com>
2023-10-17 10:58:17 -0300

    desistindo do cleancode

---
+++ testes/execs/exec03.js
@@ -0,0 +1,12 @@
+var idade = Number(window.prompt('Informe sua idade: '));
+
+document.write("Bem-vindo ao programa de cancelamneto da internet!<p>");
+document.write("Sua idade nos dirá se você ja pode se cancelado nas redes 😊");
+document.write("<p>");
+document.write('Resultado:<br>');
+
+if (idade >= 7){
+    document.write(`Você tem ${idade}! Parabéns!! Já pode ser cancelado 😊`);
+}else document.write('Muito cedo pra você, volte quando tiver uns 7 anos! 👍');
+
+
