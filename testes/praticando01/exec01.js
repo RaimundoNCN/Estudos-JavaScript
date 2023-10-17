c2b97a36d36877f96f3af205b54f81860f17a870
raimundo <raimundo.n.neto10@gmail.com>
2023-10-17 09:09:56 -0300

    exec1

---
+++ testes/praticando01/exec01.js
@@ -0,0 +1,4 @@
+var name = window.prompt('Informe um nome:');
+var idade = window.prompt('Agora uma idade: ');
+
+document.write(`Bem-vindo! ${name}, você esta sendo investigado por idade (${idade}) falsa!`)
