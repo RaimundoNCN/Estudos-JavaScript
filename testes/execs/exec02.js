ebcc66f93b46cabd8a081e630672e916f7d7cbd8
raimundo <raimundo.n.neto10@gmail.com>
2023-10-17 10:07:50 -0300

    exec02

---
+++ testes/execs/exec02.js
@@ -0,0 +1,12 @@
+var num01 = Number(window.prompt('Informe um valor: '));
+var num02 = Number(window.prompt('Informe um segundo valor: '));
+
+var soma = num01 + num02;
+var multiplo = num01 * num02;
+var subtrair = num01 - num02;
+var dividendo = num01 / num02;
+
+document.write(`Soma: ${soma} / `);
+document.write(`Multiplo: ${multiplo} / `);
+document.write(`Subtração: ${subtrair} / `);
+document.write(`Divisão: ${dividendo} `);
