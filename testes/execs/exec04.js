9f7bcb3f46fe4c94c88b3f8d478a0cca895d9417
raimundo <raimundo.n.neto10@gmail.com>
2023-10-19 16:50:09 -0300

    livros

---
+++ testes/execs/exec04.js
@@ -0,0 +1,11 @@
+const book = {
+    bookName: "Você",
+    author: "Pedro alvares cabral",
+    pageNumber: 1500,
+}
+
+
+document.write(`Livro disponivel: "${book.bookName}"`);
+console.log(book.author);
+
+
