e0194a6b9cf3bec63e53c25c37be0463efe61691
raimundo <raimundo.n.neto10@gmail.com>
2023-10-19 16:52:44 -0300

    função

---
+++ testes/execs/exec05.js
@@ -0,0 +1,12 @@
+const a = 1;
+const b = 3;
+const c = 6;
+const d = 9;
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+
+console.log(a+b);
+console.log(soma(a, b));
+console.log(soma(c, d));
