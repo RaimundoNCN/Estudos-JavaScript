12b282d025fdfe7071683028095934e6b02ebaf7
Raimundo Neto <raimundo.n.neto10@gmail.com>
2023-11-12 14:07:23 -0300

    batendo cabeça

---
+++ atrasado/dividendos.js
@@ -0,0 +1,6 @@
+function divisao(fristNum, secondNum, razao){
+    const total = fristNum/secondNum;
+    const total2 = fristNum%secondNum;
+    const result = `R: ${fristNum}/${secondNum}= ${total}`
+    const razao = total2;
+}
